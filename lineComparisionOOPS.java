373529938dd8ed1772fbde806fcfda9cebfdcec3
testForMe-Now <abcodehash@gmail.com>
2021-04-06 21:03:14 +0530

    main-> welcome to lineComparision is added

--- lineComparisionOOPS.java
+++ lineComparisionOOPS.java
@@ -1,18 +1,5 @@
-public class lineComparisionOOPS{
-	int x1,x2,y1,y2;
-	double dis;
-	lineComparisionOOPS(){
-		x1=20;
-		x2=40;
-		y1=40;
-		y2=80;
-	}
-	public void result(){
-		dis=Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-		System.out.println("the length of line between two cordinates"+"("+ x1 +","+ x2 +")"+"("+ y1 +","+ y2 +")"+"is:->"+ dis);
-	}
-	public static void main(String args[]){
-		lineComparisionOOPS object = new lineComparisionOOPS();
-		object.result();
+public class lineComparision{
+	public static void main(String arhs[]){
+		System.out.println("wlcomw to line comparision");
 	}
 }
